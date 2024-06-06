2f325bddfd5e4c95f1dcb16214429d6f9d770872
mafurman <mafurman@mail.ru>
2024-06-06 15:37:00 +0300

    swetoaor added

--- TestGitFork/ViewController.swift
+++ TestGitFork/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSvetofor: UIView!
+    @IBOutlet weak var YellowSvetofor: UIView!
+    @IBOutlet weak var GreenSvetofor: UIView!
+    
+    @IBOutlet weak var greetingButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSvetofor.layer.cornerRadius = 50
+        redSvetofor.isHidden = false
+        
+        YellowSvetofor.layer.cornerRadius = 50
+        YellowSvetofor.isHidden = false
+        
+        GreenSvetofor.layer.cornerRadius = 50
+        GreenSvetofor.isHidden = false
     }
-
-
+    
+    @IBAction func greetengButtonPressed() {
+        GreenSvetofor.isHidden.toggle()
+        YellowSvetofor.isHidden.toggle()
+        redSvetofor.isHidden.toggle()
+        if GreenSvetofor.isHidden {
+            
+        }
+        
+    }
+    
 }
 
